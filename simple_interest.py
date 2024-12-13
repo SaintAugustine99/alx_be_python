67cf074e7d012c4ae60d0ffcbda9bda0a9151260
Onserio Ogeto <onserioogeto@gmail.com>
2024-12-13 12:40:55 +0300

    Applying arithmetic and variable assignments to calculate simple interest

---
+++ simple_interest.py
@@ -0,0 +1,12 @@
+# simple_interest.py 
+
+# Defining variables
+principal = 1000
+rate = 0.05
+time = 3 
+
+# Calculating simple interest 
+interest = principal * rate * time
+
+#print the result
+print(f"The simple interest is: {interest}")
