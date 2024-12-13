94dd33b25f67a62a75a9e9180c8e9be35a0bf969
Onserio Ogeto <onserioogeto@gmail.com>
2024-12-13 13:05:54 +0300

    future age calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+# future_age_calculator.py
+
+# Prompts the user to imput their current age
+current_age = int(input("How old are you? "))
+
+# Calculate the age in 2050
+future_age = current_age + 27
+
+# Prints the results
+print(f"In 2050, you will be {future_age} years old.")
